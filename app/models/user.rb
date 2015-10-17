edb727d656709a20e077877452c7479a2e091df5
petermooibroek <petermooibroek@gmail.com>
2015-10-17 23:18:12 +0200

    Updated oauth, run db migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,16 @@
-# class User < ActiveRecord::Base
-#
-#   def self.create_with_omniauth(auth)
-#     create! do |user|
-#       user.provider = auth['provider']
-#       user.uid = auth['uid']
-#       if auth['info']
-#         user.name = auth['info']['name'] || ""
-#         user.email = auth['info']['email'] || ""
-#       end
-#     end
-#   end
-#
-# end
-
 class User < ActiveRecord::Base
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['email'] || ""
+        user.oauth_token = auth['credentials']['token'] || ""
+        user.oauth_expires_at = Time.at(auth['credentials']['expires_at']) || ""
+      end
     end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013090539) do
+ActiveRecord::Schema.define(version: 20151017211246) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "oauth_token"
+    t.string   "oauth_expires_at"
   end
 
 end
